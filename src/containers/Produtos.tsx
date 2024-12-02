0adcadb9b77fc324de908699e6c012f140382d8d
luiz <luiz@exemplo.com>
2024-12-02 18:43:27 -0300

    finalizando o uso de Redux

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,38 +1,39 @@
-import { Produto as ProdutoType } from '../App'
-import Produto from '../components/Produto'
+import { useGetProductsQuery } from '../services/api'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../store'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
+import { Produto as ProdutoType } from '../App'
+import Produto from '../components/Produto'
+import { favoritar } from '../store/reducers/carrinho'
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProductsQuery()
+  const favoritos = useSelector(
+    (state: RootReducer) => state.carrinho.favoritos
+  )
+  const dispatch = useDispatch()
+
+  if (isLoading) return <h2>Carregando...</h2>
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
-    const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    return favoritos.some((favorito) => favorito.id === produto.id)
+  }
 
-    return IdsDosFavoritos.includes(produtoId)
+  const handleFavoritar = (produto: ProdutoType) => {
+    dispatch(favoritar(produto))
   }
 
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
+            favoritar={handleFavoritar}
           />
         ))}
       </S.Produtos>
